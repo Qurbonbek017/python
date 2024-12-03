31993ebed34faab01c781316a728c07eeb3a7440
Qurbonbek017 <qurbonbektulanov777aaa@gmail.com>
2024-12-03 08:58:43 +0500

    Add files via upload

---
+++ vazifaSfetafor.py
@@ -0,0 +1,23 @@
+# import time
+# import os
+
+# while True:
+#     os.system('cls' if os.name == 'nt' else 'clear')
+#     print("🔴\n⚪️\n⚪️")
+#     time.sleep(20)
+
+#     os.system('cls' if os.name == 'nt' else 'clear')
+#     print("🔴\n🟡\n⚪️")
+#     time.sleep(5)
+
+#     os.system('cls' if os.name == 'nt' else 'clear')
+#     print("⚪️\n⚪️\n🟢")
+#     time.sleep(20)
+
+#     os.system('cls' if os.name == 'nt' else 'clear')
+#     print("⚪️\n🟡\n⚪️")
+#     time.sleep(5)
+
+
+
+
