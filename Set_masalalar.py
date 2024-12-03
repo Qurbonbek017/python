40fa00573e206aa9bd5b705e5c9b85a49c9b0297
Qurbonbek017 <qurbonbektulanov777aaa@gmail.com>
2024-12-03 08:59:12 +0500

    Add files via upload

---
+++ Set_masalalar.py
@@ -0,0 +1,85 @@
+# SET misollar 
+# set() funksiyasi ro'yxatdagi faqat unikal elementlarni olib, ularni to'plamga aylantiradi.
+# len() funksiyasi to'plamdagi elementlar sonini qaytaradi.
+
+
+
+
+#  1-masala 
+
+# testSet = {2, 3,4,3,2,-4,5,5,0}
+# print(testSet)
+
+#   2-masala
+
+# list = {1,7,9,4,5,8,90,3}
+# list1 = {9, 11, 8,5,3,2,4,7,8}
+
+# list2 = list & list1
+# print(list2)
+
+# 3- masala 
+
+# my_set = {1, 5, 6, 8, 4, 3, 6, 9}
+# my_set.remove(1)
+# print(my_set)
+
+# 4-masala
+
+# listSet = {"alisher", "rustam", 2,5,1,9,9.50,8,3}
+# listSet1 = {"sherxon", "alisher", 2,5,8,9,9.50,8,3}
+# listSet2 = {"ali", "tima", 2,5,2,0,9.40,8,3}
+# listunion = listSet, listSet1 | listSet2
+
+# print(listunion)
+
+# 5-masala
+
+# my_set = {10, 5, 20, 1,0, 15}
+
+# kichik = min(my_set)
+# print("Eng kichik element:", kichik)
+# katta = max(my_set)
+# print("Eng katta element:", katta)
+
+# 6-masala
+
+# list_set ={"azim","ulugbek","sardor"}
+# list_set.remove("azim")
+# list_set.remove("ulugbek")
+# list_set.remove("sardor")
+# list_set.add("Muhammadaziz")
+# print(list_set)
+
+# 7-masala
+
+# list_test = {1,5,7,9,0,13}
+# list_test1 = {12,6,8,5,0,3}
+
+# list_dif = list_test - list_test1
+
+# print(list_dif)
+
+#  8-masala 
+
+# import math
+
+# list_set = {2, 3, 4, 9}
+# product = math.prod(list_set)
+# print("jami: ", product)
+
+# 9-masala
+
+# my_lisset = ('')  
+
+# if not my_lisset:
+#     print("Bosh to'plam")  
+# else:
+#     print("To'plamda bosh emas!")
+
+# 10-masala
+
+# my_set = {10, 20, 30, 40, 50}
+
+# soni = len(my_set)
+# print(f"Elementlar soni: {soni}")
